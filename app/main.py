f71e226535df17a5974b7e05b6299ee7557fc0c5
Wahyu Widodo <88787416+me-dodo@users.noreply.github.com>
2024-11-27 09:18:26 +0700

    add: mount static files

--- app/main.py
+++ app/main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 from app.routes import router
 
 app = FastAPI(title="Steganography API", description="Encode and Decode Text in Images")
 
 app.include_router(router)
+# Mount the uploaded_images folder as static
+app.mount("/uploaded_images", StaticFiles(directory="uploaded_images"), name="uploaded_images")
