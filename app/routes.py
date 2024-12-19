f240f22bdc4cad136298af38a7702beff606bf68
Wahyu Widodo <88787416+me-dodo@users.noreply.github.com>
2024-12-20 00:00:59 +0700

    modify: image to original extention

--- app/routes.py
+++ app/routes.py
@@ -17,26 +17,34 @@ async def encode_text_in_image_route(
     image_file: UploadFile = File(...)
 ):
    try:
-      # Ensure the upload folder exists
-      os.makedirs(UPLOAD_FOLDER, exist_ok=True)
-        
-      # Save the uploaded image file to UPLOAD_FOLDER
-      image_path = os.path.join(UPLOAD_FOLDER, image_file.filename)
-      with open(image_path, "wb") as f:
-         f.write(await image_file.read())
+        # Ensure the upload folder exists
+        os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
-      # Encode text into the image
-      encoded_image = encode_text_into_image(image_path, text)
-        
-      # Save the encoded image as PNG
-      encoded_image_path = os.path.join(UPLOAD_FOLDER, f"encoded_{image_file.filename}")
-      encoded_image.save(encoded_image_path, format='PNG', optimize=True)
+        # Validate that the file is an image
+        valid_image_formats = {"jpeg", "jpg", "png", "bmp", "gif"}
+        file_extension = image_file.filename.split(".")[-1].lower()
+        if file_extension not in valid_image_formats:
+            raise HTTPException("Format file tidak didukung. Silahkan upload file dengan format jpeg, jpg, png, bmp, atau gif.")
 
-      data_result = EncodeResponse(imagePath=encoded_image_path)
-      result = response(200, SUCCESS_CODE, "Successfuly Encode Image", data_result)
-      return result
+        # Save the uploaded image file to UPLOAD_FOLDER
+        image_path = os.path.join(UPLOAD_FOLDER, image_file.filename)
+        with open(image_path, "wb") as f:
+            f.write(await image_file.read())
+
+        # Encode text into the image
+        encoded_image = encode_text_into_image(image_path, text)
+
+        # Save the encoded image without changing its format
+        encoded_image_path = os.path.join(UPLOAD_FOLDER, f"encoded_{image_file.filename}")
+        encoded_image.save(encoded_image_path, format=encoded_image.format, optimize=True)
+
+        # Prepare the response
+        data_result = EncodeResponse(imagePath=encoded_image_path)
+      
+        result = response(200, SUCCESS_CODE, "Successfuly Encode Image", data_result)
+        return result
    except Exception as e:
-      result = response(500, FAILES_CODE, "Failed Encode Image", str(e))
+      result = response(500, FAILED_CODE, "Failed Encode Image", str(e))
       return result
 
 @router.post("/decode")
