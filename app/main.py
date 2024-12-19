d1343bf0a3b489220be5775803aca042a42d05a9
Wahyu Widodo <88787416+me-dodo@users.noreply.github.com>
2024-12-19 23:19:01 +0700

    add: cors middleware in main.py

--- app/main.py
+++ app/main.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 from app.routes import router
 
 app = FastAPI(title="Steganography API", description="Encode and Decode Text in Images")
+
+# Add CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(router)
 # Mount the uploaded_images folder as static
